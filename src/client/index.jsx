736310923e9e376b577521084f2d9aedd0cc0c76
Niennte <niennte@gmail.com>
2018-09-18 17:37:12 -0400

    Add HMR (hot reloading modules in dev).

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,9 +1,26 @@
+/* eslint-disable global-require */
 import 'babel-polyfill';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
 
 import App from './App';
 import { APP_CONTAINER_SELECTOR } from '../shared/config';
 
-ReactDOM.render(<App />, document.querySelector(APP_CONTAINER_SELECTOR));
+const rootEl = document.querySelector(APP_CONTAINER_SELECTOR);
+
+const wrapApp = AppComponent => (
+  <AppContainer>
+    <AppComponent />
+  </AppContainer>
+);
+
+ReactDOM.render(wrapApp(App), rootEl);
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    const NextApp = require('./App').default;
+    ReactDOM.render(wrapApp(NextApp), rootEl);
+  });
+}
